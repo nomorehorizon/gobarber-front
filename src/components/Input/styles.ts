4706b0bbac6e01463b2306b4b1f80e744dd81d66
nomorehorizon <lmachado72@outlook.com>
2021-04-19 18:19:07 -0300

    input error

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -16,6 +19,10 @@ export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
 
+  ${props => props.isErrored && css`
+    border-color: #c53030;
+  `}
+ 
   ${props => props.isFocused && css`
     color: #ff9000;
     border-color: #ff9000;
@@ -24,6 +31,7 @@ export const Container = styled.div<ContainerProps>`
   ${props => props.isFilled && css`
     color: #ff9000;
   `}
+
 
   & + div {
     margin-top: 8px;
@@ -41,5 +49,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
