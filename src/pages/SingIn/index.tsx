94c7eb30e70c52a730a72b39d755a873845c7b7f
nomorehorizon <lmachado72@outlook.com>
2021-04-20 15:39:48 -0300

    context api

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useContext } from "react";
 import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
 
+import AuthContext from "../../context/AuthContext";
 import getValidationErrors from "../../utils/getValidationErrors";
 
 import logoImg from "../../assets/logo.svg";
