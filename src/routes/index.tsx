c58b9ff6a4d547ff84e62971b9512475740718db
nomorehorizon <lmachado72@outlook.com>
2021-04-27 12:57:19 -0300

    authenticated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+import Route from "./route";
 
 import SignIn from "../pages/SingIn";
 import SignUp from "../pages/SignUp";
+
+import Dashboard from "../pages/Dashboard";
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
